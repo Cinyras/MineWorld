7ed1f6848221292bc997ab4e0cf54e1bfe0da469
NATHANIEL EETEN <najaab1528@gmail.com>
2014-04-18 22:38:08 -0600

    PUSH 2.7.5.5.5.5
    added table of contents and waited for Matt

--- assets/glue.js
+++ assets/glue.js
@@ -8,12 +8,36 @@ else {
     //code for IE6, IE5
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 }
-          
+
+/*********************************************************************\
+|*********************     TABLE-OF-CONTENTS     *********************|
+|                                                                     |
+| isUsernameTaken(username):                                          |
+|   Check if the username is taken. If not, then return false. If it's|
+|   taken, return true                                                |
+|                                                                     |
+|                                                                     |
+\*********************************************************************/
+
 function isUsernameTaken(username) {
-    if (username.length != 0) {
-              document.getElementById("error-hole").innerHTML="CHECK";
-              return;
-          };
+    
+    if (username.length > 0) {
+        
+        document.getElementById("error-hole").innerHTML="CHECK";
+        return;
+        
+    };
+    
+    xmlhttp.onreadystatechange=function() {
+        
+        if (xmlhttp.readystate==4 && xmlhttp.status==200) {
+            
+            
+            return;
+            
+        }
+        
+    }
     
     xmlhttp.open("GET","assets/php/master.php?u="+username,true);
     xmlhttp.send();
