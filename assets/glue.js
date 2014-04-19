75110f5b3a18b678c63502f131c4c5fc96882e56
NATHANIEL EETEN <najaab1528@gmail.com>
2014-04-18 20:49:57 -0600

    PUSH 2.7 -NATE
    AAAAAAAAAAAAAAAAAA

---
+++ assets/glue.js
@@ -0,0 +1,20 @@
+var xmlhttp;
+if (window.XMLHttpRequest) {
+    //code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+}
+
+else {
+    //code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+          
+function isUsernameTaken(username) {
+    if (username.length != 0) {
+              document.getElementById("error-hole").innerHTML="CHECK";
+              return;
+          };
+    alert("dkfsdif");
+         // xmlhttp.open("GET","assets/php/master.php?u="+username,true);
+         // xmlhttp.send();
+}
